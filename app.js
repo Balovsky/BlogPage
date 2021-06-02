08329b2704396120f8eb78f65de3a14f04908871
Mariusz <46287724+Balovsky@users.noreply.github.com>
2021-06-02 22:08:37 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const hamb = document.querySelector('.hamburger')
 const menu = document.querySelector('.menu')
 const backBtn = document.querySelector('.back')
-hamb.addEventListener('click', ()=>{
-    hamb.classList.toggle('active')
-    menu.classList.toggle('showed')
-})
 const placeholder = document.querySelector('.placeholder')
 const btn = document.querySelector('.btnPost')
 const hovers = document.querySelectorAll('.hover')
 const imgs = document.querySelectorAll('.cover')
+const cookiesBtn = document.querySelector('.cookie__btn')
+const shadowBG = document.querySelector('.shadow')
+const cookiesBanner = document.querySelector('.cookies')
 
-
+hamb.addEventListener('click', ()=>{
+    hamb.classList.toggle('active')
+    menu.classList.toggle('showed')
+})
 // imgs.forEach(img =>{
 //     img.classList.remove('open')
 //     img.addEventListener('mouseenter', ()=>{
@@ -76,3 +78,20 @@ else
         })
     })
 }
+ 
+
+console.log(shadowBG);
+cookiesBtn.addEventListener('click', ()=>{
+        shadowBG.classList.add('hide')
+        cookiesBtn.parentElement.classList.add('hide')
+        localStorage.setItem('cookiesAccept', 'true')
+})
+
+setTimeout(() => {
+    if(!localStorage.getItem('cookiesAccept')){
+        shadowBG.classList.add('show__shadow')
+        cookiesBanner.classList.add('cookiesBanner')
+    }
+}, 2000); 
+
+// localStorage.removeItem('cookiesAccept')
