2b91ac1d3ac7ffee697514a30aa0027b98aa19d0
Mariusz <46287724+Balovsky@users.noreply.github.com>
2021-06-05 15:07:48 +0200

    Add files via upload

--- animationGSAP.js
+++ animationGSAP.js
@@ -5,16 +5,17 @@ const sections = document.querySelectorAll('section')
 
 
 
-// console.log(sections);
+console.log(sections);
 
 sections.forEach(section =>{
     gsap.fromTo(section.children, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: 0.2, duration: 1,ease: 'easeInOut', scrollTrigger: {
         trigger: section,
         start: 'top 40%',
     }})
+    // gsap.fromTo(section[2], {x: '+=500', opacity: 0}, {x: 0, opacity: 1, duration: 2, })
 })
 
-let timeline = gsap.timeline()
+let tl = gsap.timeline()
 
-timeline.fromTo('.post__column',{x: '+=500', opacity: 0}, {x: 0, opacity: 1, duration: 2, }, '+=3.5')
-        .fromTo('.post__column3',{x: '-=500', opacity: 0}, {x: 0, opacity: 1, duration: 2}, '-=2')
+// tl.fromTo('.post__column',{x: '+=500', opacity: 0}, {x: 0, opacity: 1, duration: 2, }, '+=3.5')
+// tl.fromTo('.post__column3',{x: '-=500', opacity: 0}, {x: 0, opacity: 1, duration: 2}, '-=2')
