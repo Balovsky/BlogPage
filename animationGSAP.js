37b4a630fa6358c10dda2f33069c9ad7f2a63f42
Mariusz <46287724+Balovsky@users.noreply.github.com>
2021-06-05 15:36:56 +0200

    Add files via upload

--- animationGSAP.js
+++ animationGSAP.js
@@ -11,6 +11,7 @@ sections.forEach(section =>{
     gsap.fromTo(section.children, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: 0.2, duration: 1,ease: 'easeInOut', scrollTrigger: {
         trigger: section,
         start: 'top 40%',
+        toggleActions: 'play complete none reverse',
     }})
     // gsap.fromTo(section[2], {x: '+=500', opacity: 0}, {x: 0, opacity: 1, duration: 2, })
 })
