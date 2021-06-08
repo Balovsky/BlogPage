c3cdb469e7babb6d2c17527a87bc38461ec651e6
Mariusz <46287724+Balovsky@users.noreply.github.com>
2021-06-08 18:41:43 +0200

    Add files via upload

--- animationGSAP.js
+++ animationGSAP.js
@@ -2,10 +2,10 @@
 // import {ScrollTrigger} from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 const sections = document.querySelectorAll('section')
+const hambMenu = document.querySelector('.hamburger')
+const menuItems = document.querySelectorAll('.menu__list')
 
-
-
-// console.log(sections);
+console.log(sections);
 
 sections.forEach(section =>{
     gsap.fromTo(section.children, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: 0.2, duration: 1,ease: 'easeInOut', scrollTrigger: {
@@ -15,8 +15,14 @@ sections.forEach(section =>{
     }})
     // gsap.fromTo(section[2], {x: '+=500', opacity: 0}, {x: 0, opacity: 1, duration: 2, })
 })
+let isActive = true
+hambMenu.addEventListener('click', ()=>{
+        menuTL.restart()
+})
 
-let tl = gsap.timeline()
-
-// tl.fromTo('.post__column',{x: '+=500', opacity: 0}, {x: 0, opacity: 1, duration: 2, }, '+=3.5')
-// tl.fromTo('.post__column3',{x: '-=500', opacity: 0}, {x: 0, opacity: 1, duration: 2}, '-=2')
+let menuTL = gsap.timeline()
+    menuTL.set(menuItems, {autoAlpha: 0})
+    menuTL.fromTo(menuItems[0], {y: '+=500'}, {y: 0, autoAlpha: 1, duration: 1, ease: 'back'})
+    menuTL.fromTo(menuItems[1], {y: '+=500'}, {y: 0, autoAlpha: 1, duration: 1, ease: 'back', }, '-=0.75')
+    menuTL.fromTo(menuItems[2], {y: '+=500'}, {y: 0, autoAlpha: 1, duration: 1 , ease: 'back', }, '-=0.75')
+    menuTL.fromTo(menuItems[3], {y: '+=500'}, {y: 0, autoAlpha: 1, duration: 1 , ease: 'back', }, '-=0.75')
